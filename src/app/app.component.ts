885ab2a958e2d7e77229e8d9189058b693b3c40c
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-06-08 19:33:19 -0300

    feat: restructure app component and update index.html for improved layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, FooterComponent],
+  template: `
+    <app-nav></app-nav>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'Shiakan-Frontend';
+  name = 'Angular';
 }
