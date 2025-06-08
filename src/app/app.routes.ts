8016246259c18c1714744a2b0e9c7ae467cb4567
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-06-08 19:33:33 -0300

    feat: add server-side routing and update application configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { CompetitorsComponent } from './modules/competitors/competitors.component';
+import { CompetitorDetailComponent } from './modules/competitors/competitor-detail.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'competidores',
+    component: CompetitorsComponent,
+  },
+  {
+    path: 'competidor/:documentId',
+    component: CompetitorDetailComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
